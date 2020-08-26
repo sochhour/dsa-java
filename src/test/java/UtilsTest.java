a1681c19b2fc474d28d4739b5d421b4d588d116a
Sophie-Chhour-Kaplan <sophie.chor.kaplan@emory.edu>
2020-08-25 21:10:13 -0400

    build successful

---
+++ src/test/java/UtilsTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class UtilsTest {
+    @Test
+    public void getMiddleIndexTest() {
+        assertEquals(5, Utils.getMiddleIndex(0, 10));
+    }
+}
